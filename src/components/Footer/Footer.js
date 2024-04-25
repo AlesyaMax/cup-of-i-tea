3954ebb2fb0e76c86ab9c5ca0920e2b8bd08ce39
Alesya Maximenko <a-lesya.net@mail.ru>
2024-04-26 02:31:34 +0300

    add Header, Footer & edit ReadMe

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+function Footer(props) {
+  return (
+    <footer className='footer'>
+      <p className='footer__author'>© AlesyaMax</p>
+      <p className='footer__year'>2024</p>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+function Header(props) {
+  return(
+    <header className='header'>
+      <h1 className='header__title'>What is fun for you<div className='header__title_span_question'>?</div>
+      </h1>
+    </header>
+  )
+}
+
+export default Header;
